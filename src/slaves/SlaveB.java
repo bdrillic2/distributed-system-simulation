0201674fdf908bc547042c22778ec94ad2a2166f
DevorahTeitelbaum <154083635+DevorahTeitelbaum@users.noreply.github.com>
2024-11-17 05:49:08 -0500

    created basic code for slaves

---
+++ src/slaves/SlaveB.java
@@ -0,0 +1,44 @@
+package src.slaves;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.concurrent.TimeUnit;
+
+class SlaveB {
+	public static void main(String[] args) throws InterruptedException {
+		// will connect here
+		if (args.length != 2) {
+			System.err.println("Usage: java clientCode <host name> <port number>");
+			System.exit(1);
+		}
+
+		String hostName = args[0];
+		int portNumber = Integer.parseInt(args[1]);
+
+		// attempts to connect to socket
+		try (Socket echoSocket = new Socket(hostName, portNumber);
+				PrintWriter out = new PrintWriter(echoSocket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(echoSocket.getInputStream()));
+				BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+			String job = "job";
+			job = in.readLine();
+			if (job.equals("B")) {
+				TimeUnit.SECONDS.sleep(2);
+			} else if (job.equals("A")) {
+				TimeUnit.SECONDS.sleep(10);
+			} else {
+				throw new UnknownJobException();
+			}
+		} catch (UnknownHostException e) {
+			System.err.println("Don't know about host " + hostName);
+			System.exit(1);
+		} catch (IOException e) {
+			System.err.println("Couldn't get I/O for the connection to " + hostName);
+			System.exit(1);
+		}
+	}
+}
